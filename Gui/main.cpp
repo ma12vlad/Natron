ee32ccc672ea05c04eeed617823ddeb2e03aafe1
Vladislav <ma12@rambler.ru>
2024-06-28 14:15:03 +0300

    Add files via upload

--- Gui/main.cpp
+++ Gui/main.cpp
@@ -1,24 +1,17 @@
 
 #include <QtCore/QTranslator>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     QApplication app(argc, argv);
-            // Changes the file loaded from the resource container
-            // to force a different language
-            if (arg == "--ru") {
-                QLocale::setDefault(QLocale::Russian);
-                langUpdated = true;
-            } else if (arg == "--no-stdout") {
-                logToStdout = false;
-            } else {
-                langUpdated = false;
-            }
 
-            if (langUpdated) {
-                qDebug() << "Changed locale to " << QLocale::languageToString(QLocale().language());
-            } else {
-    // Load the translation file from the resource container and use it
+    QTranslator qtTranslator;
+    qtTranslator.load(QLocale::Russian, QString("qt_") + locale, QLibraryInfo::location(QLibraryInfo::TranslationsPath));
+    app.installTranslator(&qtTranslator);
+
     QTranslator translator;
     translator.load(":translations");
     QApplication::installTranslator(&translator);
+
+    return app.exec();
 }
